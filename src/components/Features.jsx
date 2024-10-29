de5b4c36cc185ef872f2c60f09dfe13372b2babc
kaushik <helworld6@gmail.com>
2024-10-29 19:49:16 +0545

    add three new image with animation

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,10 +1,17 @@
 import { useGSAP } from "@gsap/react"
 import { animateWithGsap } from "../utils/animation";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
+import { useRef } from "react";
 
 
 const Features = () => {
+    const videoRef = useRef();
     useGSAP(() => {
         animateWithGsap('#features-title', {y:0, opacity:1})
+        animateWithGsap('.g_grow',{
+            scale:1, opacity:1, ease: 'power'
+        }, {scrub: 5.5}
+    )
     }, []);
     
     return (
@@ -17,6 +24,28 @@ const Features = () => {
             <div className="flex flex-col justify-center items-center overflow-hidden">
                 <div className="mt-32 mb-24 pl-24">
                     <h2 className="text-5xl lg:text-7xl font-semibold">iPhone.</h2>
+                    <h2 className="text-5xl lg:text-7xl font-semibold">Forged in titanium.</h2>
+                </div>
+
+                <div className="flex-center flex-col sm:px-10">
+                    <div className="relative h-[50vh] w-full flex items-center">
+                        <video playsInline id='exploreVideo' className="w-full h-full object-cover
+                            object-center" preload="none" muted autoPlay ref={videoRef}>
+                            <source src={exploreVideo} type="video/mp4" />
+                        </video>
+                    </div>
+
+                    <div className="flex flex-col w-full relative">
+                        <div className="feature-video-container">
+                            <div className="overflow-hidden flex-1 h-[50vh]">
+                                <img src={explore1Img} alt="titanium" className="feature-video g_grow" />
+                            </div>
+                            <div className="overflow-hidden flex-1 h-[50vh]">
+                                <img src={explore2Img} alt="titanium 2" className="feature-video g_grow" />
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
